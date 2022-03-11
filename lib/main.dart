d3b63781ddd4c47112cf2de26e2f452369b5d02c
tarafat <tanvirarafat02@gmail.com>
2022-03-11 15:06:39 +0600

    internet permision

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:contesta_na_hora/constants/custome_theme.dart';
 import 'package:contesta_na_hora/controller/contasa.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
@@ -11,6 +12,11 @@ import 'home_navigation_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+    ],
+  );
   runApp(const MyApp());
 }
 
